9f6bb20acb8f7edc716c26c0f7e41d0040cf8e81
Kashav Madan <kshvmdn@gmail.com>
2016-12-28 17:44:45 -0500

    Use new structure, add team logos

--- data/fetch.sh
+++ data/fetch.sh
@@ -2,5 +2,5 @@
 
 python3 ./fetch.py
 find player_data -name '*.json' -exec cat {} \; | jq --slurp . -c > players.json
-mkdir -p ../docs/data; mv players.json $_/
+mkdir -p ../docs/assets/data; mv players.json $_
 rm -rf player_data
---
+++ data/logos.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+for TEAM in "ATL" "BOS" "BKN" "CHA" "CHI" "CLE" "DAL" "DEN" "DET" "GSW" "HOU" "IND" "LAC" "LAL" "MEM" "MIA" "MIL" "MIN" "NOP" "NYK" "OKC" "ORL" "PHI" "PHX" "POR" "SAC" "SAS" "TOR" "UTA" "WAS"
+do
+  wget "http://stats.nba.com/media/img/teams/logos/${TEAM}_logo.svg"
+  cairosvg ${TEAM}_logo.svg -o ${TEAM}.png
+  convert ${TEAM}.png -resize 256x256 ${TEAM}.png
+done
+
+rm *.svg
+mkdir -p ../docs/assets/images/teams; mv *.png $_
